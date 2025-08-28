7e92b469cacfb35ddaebb17bc8360bdfb09b19e4
Codeword-Agency <alux@codewordagency.com>
2025-08-28 16:22:32 -0400

    oush to publish
    push to publish

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,8 @@ import VHSOverlay from "@/components/vhs-overlay"
 import CRTScreen from "@/components/crt-effect"
 
 export const metadata: Metadata = {
-  title: "v0 App",
-  description: "Created with v0",
-  generator: "v0.app",
+  title: "66.6FM Horror Hotline",
+  description: "Share your worst data horror story for a chance to win prizes, including tickets to NYC Advertising Week.",
 }
 
 export default function RootLayout({
@@ -18,6 +17,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head></head>
+      <meta property="og:image" content="/socials/og-share.png" />
+      <meta property="og:image:alt" content="66.6FM Horror Hotline" />
+      <meta property="og:image:type" content="image/png" />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="628" />
+
+      <meta name="twitter:image" content="/socials/og-share.png" />
+      <meta name="twitter:image:alt" content="66.6FM Horror Hotline" />
+      <meta name="twitter:image:type" content="image/png" />
+      <meta name="twitter:image:width" content="1200" />
+      <meta name="twitter:image:height" content="628" />
       <body>
         <CRTScreen />
         {children}
