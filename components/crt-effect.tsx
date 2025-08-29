db8d404afa58b5e92a1aa6a1aaf08ffd3886d891
Codeword-Agency <alux@codewordagency.com>
2025-08-29 11:25:59 -0400

    Update crt-effect.tsx
    testing this out

--- components/crt-effect.tsx
+++ components/crt-effect.tsx
@@ -294,7 +294,7 @@ export default function CRTOverlay() {
   const screenRef = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
-    if (!screenRef.current) return
+    if (!screenRef.current && window.innerWidth < 600) return
 
     const screen = new ScreenEffect(screenRef.current, {})
 
