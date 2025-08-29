9113bcae4d7af0f7cd636c4346e64049d8e88f7d
Codeword-Agency <alux@codewordagency.com>
2025-08-29 12:06:26 -0400

    Update page.tsx
    fixing style error

--- app/page.tsx
+++ app/page.tsx
@@ -157,7 +157,7 @@ export default function ContactForm() {
       <div className="headline-wrap">
           <img src="/LOGO-NEW.svg" />
           <div className="tune-in">
-              <iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/playlist/1yXUDcTJ8KWTYyclJjgmy1?utm_source=generator&theme=0" width="100%" height="152" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+              <iframe data-testid="embed-iframe" src="https://open.spotify.com/embed/playlist/1yXUDcTJ8KWTYyclJjgmy1?utm_source=generator&theme=0" width="100%" height="152" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
             </div>
           </div>
         </div>
